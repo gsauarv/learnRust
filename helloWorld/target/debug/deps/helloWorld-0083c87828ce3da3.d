6f533866b6fdc0b845834ffa3ce3b2400d3217e7
gsauarv <gsauarv>
2021-10-07 22:04:44 +0545

    cmd:Cargo build

---
+++ helloWorld/target/debug/deps/helloWorld-0083c87828ce3da3.d
@@ -0,0 +1,5 @@
+/home/sg/Desktop/learnRust/helloWorld/target/debug/deps/helloWorld-0083c87828ce3da3: src/main.rs
+
+/home/sg/Desktop/learnRust/helloWorld/target/debug/deps/helloWorld-0083c87828ce3da3.d: src/main.rs
+
+src/main.rs:
---
+++ helloWorld/target/debug/helloWorld.d
@@ -0,0 +1,1 @@
+/home/sg/Desktop/learnRust/helloWorld/target/debug/helloWorld: /home/sg/Desktop/learnRust/helloWorld/src/main.rs
